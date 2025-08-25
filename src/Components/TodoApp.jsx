7e1c223269d939ddf309bc2bf956ba924165b638
tais1719 <taissantos123098@gmail.com>
2025-08-24 21:52:30 -0300

    git commit -m " Salva no localStorage sempre que tasks mudar"

--- src/Components/TodoApp.jsx
+++ src/Components/TodoApp.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import '../Styles/index.css';
 import Abertura from '../assets/foto22.avif';
-import TodoIten from '../Components/TodoIten';
+import TodoList from '../Components/TodoList';
 import { Rocket } from '@phosphor-icons/react';
 
 // Página inicial
@@ -10,14 +10,12 @@ function Home() {
 
   return (
     <div className="app-container">
-      {/* Ícone no topo */}
-      {/* Ícone no topo */}
-      <Rocket  weight="fill"className="top-icon" />
-
-
+      <Rocket weight="fill" className="top-icon" />
       <img src={Abertura} alt="Todo List" className="app-image" />
       <h1 className="todo-title">Organize Seu Dia com Nossa Todo List</h1>
-      <button className='botao1' onClick={() => navigate('/todoiten')}>Vamos começar</button>
+      <button className='botao1' onClick={() => navigate('/todolist')}>
+        Vamos começar
+      </button>
     </div>
   );
 }
@@ -28,7 +26,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/todoiten" element={<TodoIten />} />
+        <Route path="/todolist" element={<TodoList />} />
       </Routes>
     </Router>
   );
